992eef0ea52ae16ae489dbca497a15ce7b5257df
culpepperd <culpepperd2490@gmail.com>
2021-07-22 11:03:26 -0400

    Adjusted Scripts
    Adjusted scripts

--- Unity_Learn1/Assets/Scripts/Game.cs
+++ Unity_Learn1/Assets/Scripts/Game.cs
@@ -1,29 +1,22 @@
-﻿using System.Collections.Generic;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Game : MonoBehaviour
 {
-    //Player myPlayer = new Player();
+    Player myPlayer = new Player();
+
 
     void Start()
     {
-        Debug.Log("This is fucking rediculus.");
-        //SayThis();
-        // PrintToScreen();
+        PrintThis();
     }
 
-    /*
-    private void PrintToScreen()
+    private void PrintThis()
     {
         myPlayer.Experience = 5;
         int x = myPlayer.Experience;
 
-        Debug.Log("Heyyyy: " + x);
-    }
-    */
-    private void SayThis()
-    {
-        Debug.Log("Hellooooo");
+        Debug.Log(x);
     }
 }
