8db3b8d4cd4f9bff9d26c445d65d6ea099658811
culpepperd <culpepperd2490@gmail.com>
2021-07-25 07:54:00 -0400

    Update
    Update

--- Unity_Learn1/Assets/Scripts/Static Variables/Enemy.cs
+++ Unity_Learn1/Assets/Scripts/Static Variables/Enemy.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/* Static members (such as variables and methods) are members that 
+ * are shared amongst all instances of a class.
+ * 
+ * Furthermore, static members can be accessed directly from a class
+ * without instantiating an object of the class first. 
+ *  
+ * Normally, member variables are unique to each object of a class, 
+ * while every object of the class has the same variables, 
+ * they each have their own values.
+ * 
+ * However, with static variables, 
+ * 
+ */
+
+
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
